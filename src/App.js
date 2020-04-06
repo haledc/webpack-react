07e5d308539cd420d0ff9f451741432ff65d3b09
Hale Deng <haledeng88@gmail.com>
2020-04-06 17:22:47 +0800

    refactor: update router config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Redirect } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './containers/Home'
 import About from './containers/About'
@@ -8,8 +8,15 @@ const App = () => (
   <>
     <BrowserRouter>
       <Nav />
-      <Route path="/home" component={Home} />
-      <Route path="/about" component={About} />
+      <Route path="/">
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
     </BrowserRouter>
   </>
 )
