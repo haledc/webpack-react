5f0bdf46b2ffb891f5d1b66c22303f7d5c0928e0
Hale Deng <haledeng88@gmail.com>
2020-04-06 19:24:49 +0800

    refactor: update Route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Redirect } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './containers/Home'
 import About from './containers/About'
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <div>
-      <Nav />
-      <Route path="/home" component={Home} />
-      <Route path="/about" component={About} />
-    </div>
+    <Nav />
+    <Route path="/">
+      <Redirect to="/home" />
+    </Route>
+    <Route path="/home">
+      <Home />
+    </Route>
+    <Route path="/about">
+      <About />
+    </Route>
   </BrowserRouter>
 )
 
