c3756d772f426af0cd2898cdcd2a55a0d41a38e2
Hale Deng <haledeng88@gmail.com>
2020-06-04 12:20:51 +0800

    refactor: rename

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const baseConfig = require("./webpack.config");
+
+const webpackDevConfig = merge(baseConfig, {
+  mode: "development",
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    host: "127.0.0.1",
+    port: 3000,
+    hot: true,
+    hotOnly: true,
+    overlay: {
+      errors: true,
+    },
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "../template.html"),
+      filename: "index.html",
+    }),
+  ],
+});
+
+module.exports = webpackDevConfig;
---
+++ build/webpack.config.prod.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const baseConfig = require("./webpack.config");
+
+const webpackProdConfig = merge(baseConfig, {
+  mode: "production",
+  output: {
+    filename: "js/[name].[contenthash:8].js",
+    chunkFilename: "js/[name].thunk.[contenthash:8].js",
+  },
+  devtool: "cheap-module-source-map",
+  optimization: {
+    runtimeChunk: {
+      name: "runtime",
+    },
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "../template.html"),
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeAttributeQuotes: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+});
+
+module.exports = webpackProdConfig;
